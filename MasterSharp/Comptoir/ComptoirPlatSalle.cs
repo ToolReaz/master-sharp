393afee237d397e82affd7a9f779265ab4e275ad
Aymeric <aymeric.lemoine@viacesi.fr>
2018-12-05 16:09:51 +0100

    Create all classe from"Comptoir"

---
+++ MasterSharp/Comptoir/ComptoirPlatSalle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterSharp.Comptoir
+{
+    class ComptoirPlatSalle : IComptoir
+    {
+        public void AddElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveElement()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
