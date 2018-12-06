9bd4849d5ef08d04e118b24007424c9c2fa530f7
ToolReaz <toolreaz@outlook.fr>
2018-12-06 10:50:29 +0100

    Create Test Project

---
+++ MasterSharpUnitTests/Model/Stock/AlimentTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MasterSharpUnitTests.Model.Stock
+{
+    [TestClass()]
+    public class AlimentTests
+    {
+        [TestMethod()]
+        public void AlimentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsFreshTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ MasterSharpUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MasterSharpUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MasterSharpUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35a00312-1e37-4454-a735-f803e9f03855")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
