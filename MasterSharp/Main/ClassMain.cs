8ae5f0f3da936c8789d934cf5192d53ef56aa7b1
VictorLosser <victor.losser@viacesi.fr>
2018-12-07 12:19:43 +0100

    Correction of the Entity Model Designer

---
+++ MasterSharp/Main/ClassMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main
+{
+    class ClassMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+
+        }
+    }
+}
