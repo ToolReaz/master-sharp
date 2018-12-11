3423e3cda81440f2759610c64b12d42547c76f34
Aymeric <aymeric.lemoine@viacesi.fr>
2018-12-11 11:00:37 +0100

    create thread and generate client

--- MasterSharp/Model/Salle/MaitreHotel.cs
+++ MasterSharp/Model/Salle/MaitreHotel.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using MasterSharp.Model.Salle;
 
 namespace Model.Salle
 {
     public class MaitreHotel : IEmployeSalle
     {
+        private Salle salle;
+        private Thread thread;
+        private Table table;
+
+        public MaitreHotel(Salle salle)
+        {
+            this.salle = salle;
+            this.thread = new Thread(new ThreadStart(this.DoWork));
+        }
         public void DoWork()
         {
+            
             throw new NotImplementedException();
         }
 
