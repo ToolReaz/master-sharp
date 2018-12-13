3e42e5da38e44957b6cd772fe0b88e1b1a23bd41
ToolReaz <toolreaz@outlook.fr>
2018-12-13 08:58:07 +0100

    fix recettes

--- MasterSharp/Model/Stock/EtapeRecette.cs
+++ MasterSharp/Model/Stock/EtapeRecette.cs
@@ -5,14 +5,12 @@ namespace MasterSharp.Model.Stock
 {
     public class EtapeRecette
     {
-        public IVaisselle Vaisselle { get; }
         public Aliment Aliment { get; }
         public ActionRecette Action { get; }
         public bool Completed { get; }
 
 
-        public EtapeRecette(IVaisselle vaisselle, Aliment aliment, ActionRecette action) {
-            this.Vaisselle = vaisselle;
+        public EtapeRecette(Aliment aliment, ActionRecette action) {
             this.Aliment = aliment;
             this.Action = action;
             this.Completed = false;
